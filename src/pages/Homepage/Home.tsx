f12775d53e598b148fb7942f01c3ec69733525d0
Rafaelpena31 <rafaelppena31@gmail.com>
2020-08-28 22:53:46 -0300

    add modal - RP

--- src/pages/Homepage/Home.tsx
+++ src/pages/Homepage/Home.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import 'antd/dist/antd.css';
 import './Home.scss';
 
 import Header from '../../components/Header/Header';
@@ -7,12 +8,52 @@ import CurrencyTable from '../../components/CurrencyTable/CurrencyTable';
 
 import ardorIcon from '../../assets/icons/ardor.png';
 
+import { Modal, Button } from 'antd';
+
 
 function Homepage() {
+
+  const [loading, setLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  function showModal() {
+    setVisible(true);
+  }
+
+  function handleOk() {
+    setLoading(true);
+    setTimeout(() =>( 
+      setLoading(false), setVisible(false)
+    ), 3000);
+  }
+
+  function handleCancel() {
+    setVisible(false);
+  };
+
   return (
     <div className="homepage">
       <Header>
-         <button type="button">New Cryptocurrency+</button>
+         <button type="button" onClick={showModal}>New Cryptocurrency+</button>
+         <>
+          <Modal
+            visible={visible}
+            title="New Cryptocurrency"
+            onOk={handleOk}
+            onCancel={handleCancel}
+            footer={[
+              <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
+                Add
+              </Button>,
+            ]}
+          >
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </Modal>
+         </>
       </Header>
       <main>
         <div className="table-currency">
