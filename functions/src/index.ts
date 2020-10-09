0d3ac092bcd0776acecc38f7f1d3867e1e8c08bc
Rafaelpena31 <rafaelppena31@gmail.com>
2020-10-09 20:00:11 -0300

    Backend with node and cloud function add

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,8 +6,36 @@ const app: express.Application = express();
 
 admin.initializeApp();
 const db = admin.firestore();
+/* Users */
+app.post("/users", (request, response) => {
+  db.collection("users")
+    .doc(request.body.uid)
+    .set({
+      email: request.body.email,
+      id: request.body.uid,
+      name: request.body.name,
+    })
+    .then((i) => {
+      response.status(200).json(i);
+    })
+    .catch((e) => {
+      response.json(e);
+    });
+});
 
-app.post("/wallets", (request, response) => {
+app.get("/users", (request, response) => {
+  db.collection("users")
+    .doc(request.body.uid)
+    .get()
+    .then((user) => {
+      response.status(200).json(user);
+    })
+    .catch((e) => {
+      response.json(e);
+    });
+});
+/* Wallets */
+app.post("/wallet", (request, response) => {
   db.collection("wallets")
     .add({
       id: request.body.uid,
@@ -21,14 +49,28 @@ app.post("/wallets", (request, response) => {
       response.json(e);
     });
 });
+app.put("/walletAdd", (request, response) => {
+  db.collection("/wallets")
+    .doc(request.body.walletId)
+    .update({
+      coins: request.body.coins,
+      totalValue: request.body.totalValue,
+    })
+    .then((i) => {
+      response.status(200).json(i);
+    })
+    .catch((e) => {
+      response.json(e);
+    });
+});
+app.put("/walletSell", (request, response) => {
+  db.collection("wallet");
 
-app.post("/users", (request, response) => {
-  db.collection("users")
-    .doc(request.body.uid)
-    .set({
-      email: request.body.email,
-      id: request.body.uid,
-      name: request.body.name,
+  db.collection("/wallets")
+    .doc(request.body.walletId)
+    .update({
+      coins: request.body.coins,
+      totalValue: request.body.totalValue,
     })
     .then((i) => {
       response.status(200).json(i);
